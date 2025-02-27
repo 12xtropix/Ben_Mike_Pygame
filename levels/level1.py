1817947d20d01f6d5b933586be0bc6e9ee5a6ee3
michaelgiuliano <mcgiuliano@potomacschool.org>
2025-02-27 14:32:07 -0500

    finish line

--- levels/level1.py
+++ levels/level1.py
@@ -5,16 +5,23 @@ from objects.door import Door
 class Level1:
     def __init__(self):
         self.platforms = [
-            pygame.Rect(50, config.FLOOR, 1000, 20),
+            pygame.Rect(50, config.FLOOR, 100, 20), # pyramid platforms
             pygame.Rect(200, 450, 100, 20),
             pygame.Rect(400, 350, 100, 20),
-            pygame.Rect(600, 250, 100, 20)
+            pygame.Rect(600, 250, 100, 20),
+            pygame.Rect(800, 150, 100, 20),
+            pygame.Rect(1000, 250, 100, 20),
+            pygame.Rect(1200, 350, 100, 20), # pyramid platforms
+            pygame.Rect(1400, 450, 600, 20),
+
+            pygame.Rect(1700, 150, 20, 300), # wall
+
         ]
         self.moving_platforms = [
 
         ]
-        self.ground = [pygame.Rect(50, config.FLOOR, 1000, 20)]
-        self.door = Door(900, 200)
+        self.ground = [pygame.Rect(50, config.FLOOR, 100, 20)]
+        self.door = Door(3000, 200)
 
 
     def draw(self, screen, camera):
